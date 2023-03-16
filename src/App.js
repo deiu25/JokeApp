88e8fc52a6fd70b9512472992d8c2ccf09503c69
deiu25 <d3iu25@gmail.com>
2023-03-16 23:11:10 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import VoiceRSS from "./components/VoiceRSS";
+
+class JokeApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      joke: "",
+    };
+  }
+
+  renderJoke = (joke) => {
+    this.setState({ joke });
+  };
+
+  getJokes = async () => {
+    const url = "https://v2.jokeapi.dev/joke/Any";
+
+    let joke = "";
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.setup) {
+        joke = `${data.setup} ... ${data.delivery}`;
+      } else {
+        joke = data.joke;
+      }
+      console.log(data);
+      console.log(joke);
+    } catch (e) {
+      console.log(e);
+    }
+    this.renderJoke(joke);
+    this.tellMeAJoke(joke);
+  };
+
+  tellMeAJoke = (joke) => {
+    VoiceRSS.speech({
+      key: "5ff4b1b6ab8143f488a977bc3a2f2a9c",
+      src: joke,
+      hl: "en-us",
+      v: "Linda",
+      r: 0,
+      c: "mp3",
+      f: "44khz_16bit_stereo",
+      ssml: false,
+    });
+  };
+
+  render() {
+    const { joke } = this.state;
+
+    return (
+      <div>
+        <button id="button" onClick={this.getJokes}>
+          Get Jokes
+        </button>
+        <div id="speech">{joke}</div>
+      </div>
+    );
+  }
+}
+
+export default JokeApp;
---
+++ src/components/VoiceRSS.js
@@ -0,0 +1,94 @@
+class VoiceRSS {
+  static speech(settings) {
+    this._validate(settings);
+    this._request(settings);
+  }
+
+  static _validate(settings) {
+    if (!settings) throw new Error("The settings are undefined");
+    if (!settings.key) throw new Error("The API key is undefined");
+    if (!settings.src) throw new Error("The text is undefined");
+    if (!settings.hl) throw new Error("The language is undefined");
+    if (settings.c && "auto" !== settings.c.toLowerCase()) {
+      let supported = false;
+      switch (settings.c.toLowerCase()) {
+        case "mp3":
+          supported = new Audio().canPlayType("audio/mpeg").replace("no", "");
+          break;
+        case "wav":
+          supported = new Audio().canPlayType("audio/wav").replace("no", "");
+          break;
+        case "aac":
+          supported = new Audio().canPlayType("audio/aac").replace("no", "");
+          break;
+        case "ogg":
+          supported = new Audio().canPlayType("audio/ogg").replace("no", "");
+          break;
+        case "caf":
+          supported = new Audio().canPlayType("audio/x-caf").replace("no", "");
+          break;
+      }
+      if (!supported)
+        throw new Error(
+          "The browser does not support the audio codec " + settings.c
+        );
+    }
+  }
+
+  static _request(settings) {
+    const params = this._buildRequest(settings);
+    const xhr = this._getXHR();
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        if (xhr.responseText.startsWith("ERROR"))
+          throw new Error(xhr.responseText);
+        new Audio(xhr.responseText).play();
+      }
+    };
+
+    xhr.open("POST", "https://api.voicerss.org/", true);
+    xhr.setRequestHeader(
+      "Content-Type",
+      "application/x-www-form-urlencoded; charset=UTF-8"
+    );
+    xhr.send(params);
+  }
+
+  static _buildRequest(settings) {
+    const codec =
+      settings.c && "auto" !== settings.c.toLowerCase()
+        ? settings.c
+        : this._detectCodec();
+    return `key=${settings.key || ""}&src=${settings.src || ""}&hl=${
+      settings.hl || ""
+    }&v=${settings.v || ""}&r=${settings.r || ""}&c=${codec || ""}&f=${
+      settings.f || ""
+    }&ssml=${settings.ssml || ""}&b64=true`;
+  }
+
+  static _detectCodec() {
+    const audio = new Audio();
+    return audio.canPlayType("audio/mpeg").replace("no", "")
+      ? "mp3"
+      : audio.canPlayType("audio/wav").replace("no", "")
+      ? "wav"
+      : audio.canPlayType("audio/aac").replace("no", "")
+      ? "aac"
+      : audio.canPlayType("audio/ogg").replace("no", "")
+      ? "ogg"
+      : audio.canPlayType("audio/x-caf").replace("no", "")
+      ? "caf"
+      : "";
+  }
+
+  static _getXHR() {
+    try {
+      return new XMLHttpRequest();
+    } catch (e) {
+      throw new Error("The browser does not support HTTP request");
+    }
+  }
+}
+
+export default VoiceRSS;
